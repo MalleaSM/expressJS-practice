5a0ce8f90846442be963978857e986a016e2360a
SergioMM <sergiomallea80@gmail.com>
2023-07-28 20:36:05 -0400

    node-localstorage test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import diaryRouter from './routes/diaries'
 import dotenv from 'dotenv';
+import LocalStorage from 'node-localstorage';
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,29 @@ app.get('/api/ping', (_req, res) => {
     console.log('ping here');
     res.send('pong');
 })
+
+app.get('/store/:key/:value', (req, res) => {
+    const { key, value } = req.params;
+    const localStorage = new LocalStorage.LocalStorage('./scratch');
+  
+    localStorage.setItem(key, value);
+  
+    res.send(`Value '${value}' stored with key '${key}' in localStorage.`);
+  });
+  
+  // Set up a route to retrieve data
+  app.get('/retrieve/:key', (req, res) => {
+    const { key } = req.params;
+    const localStorage = new LocalStorage.LocalStorage('./scratch');
+    const value = localStorage.getItem(key);
+  
+    if (value) {
+      res.send(`Value for key '${key}': ${value}`);
+    } else {
+      res.send(`No value found for key '${key}'`);
+    }
+  });
+  
 
 app.all('/', (_req, res) => {
     const key = process.env.KEY;
